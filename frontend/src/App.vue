532e0a9f8667d9e01fecc196ebeaab34038e3719
Andy Pfister <andy.pfister@simplificator.com>
2021-04-24 11:51:45 +0200

    Add Bulma and Font Awesome

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Hello World</h1>
+      <p class="subtitle">
+        My first website with
+        <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import '../node_modules/bulma/css/bulma.css';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld,
-  },
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
